b2d840186643dacd30c4cf3c3b38b3576f813c8e
Akash Levy <akashlevy@gmail.com>
2019-07-25 23:32:08 -0700

    Updates and add CIFAR10 file

--- mnist_cnn.py
+++ mnist_cnn.py
@@ -64,12 +64,15 @@ model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
 
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(x_test, y_test))
-model.save_weights('mnist_cnn.h5')
+if mode == 'train':
+    model.fit(x_train, y_train,
+            batch_size=batch_size,
+            epochs=epochs,
+            verbose=1,
+            validation_data=(x_test, y_test))
+    model.save_weights('mnist_cnn.h5')
+elif mode == 'load':
+    mode.load_weights('mnist_cnn.h5')
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
