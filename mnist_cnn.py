ade69ac95a2b704e7ecdd83296613c1021d67a39
Akash Levy <akashlevy@gmail.com>
2019-07-25 23:15:53 -0700

    Small update

--- mnist_cnn.py
+++ mnist_cnn.py
@@ -4,10 +4,11 @@ Gets to 99.25% test accuracy after 12 epochs
 16 seconds per epoch on a GRID K520 GPU.
 '''
 
+from __future__ import print_function
+
 import os
 os.environ["TF_XLA_FLAGS"]="--tf_xla_cpu_global_jit"
 
-from __future__ import print_function
 import keras
 from keras.datasets import mnist
 from keras.models import Sequential
